94a8570d2e417dde54941a29a3dcfa07b09797c1
Rich <Rich@walkingd3ad.attlocal.net>
2017-02-01 21:43:46 -0500

    Added search-it.ps1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Powershell-Util
+
 Powershell-Util is a collection of Powershell scripts.
+
+## search-it.ps
+
+This script will search a directory recursively for files with the specified extension(s).  With the found files a search will be completed for the supplied string.
+
---
+++ search-it.ps1
@@ -0,0 +1,44 @@
+﻿param (
+    [string] $searchin,  # Full Path to start search in
+    [string] $wordlist,  # Word list of terms to search for
+    [string] $ext,       # Comma delimited string of file extensions.  (aspx,html,jsp)
+    [string] $word       # Single term to search for 
+)
+
+
+function SearchForIt($line)
+{
+    $tblFormat = @{Expression={$_.Filename};Label="Resource";width=20},
+                 @{Expression={$_.LineNumber};Label="Line";width=10},
+                 @{Expression={$_.Line -replace '^\s+',''};Label="Text";width=400}
+
+    ""
+    ""
+    Write-Host $line
+    Write-Host '-----------------------------------------------------'
+    $a = Get-ChildItem -Path $searchin -Include $fileExtensions -Recurse | Select-String "$line" -SimpleMatch
+    if($a.count -gt 0) { $a | Format-Table -Wrap $tblFormat }
+    $a = ""
+}
+
+# Convert the file extension string to an array and prepend
+# each array item with the wild card characters.
+[array]$fileExtensions = $ext.split(",")
+
+for ( $i = 0; $i -lt $fileExtensions.Count; $i++ ) {
+  $fileExtensions[$i] = "*.$($fileExtensions[$i])"
+}
+
+# Perform searches on wordlist and/or single word
+if($wordlist -ne '')
+{
+    foreach($line in Get-Content $wordlist)
+    {
+        SearchForIt $line
+    }
+}
+
+if($word -ne '')
+{
+    SearchForIt $word
+}
